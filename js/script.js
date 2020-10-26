672cfaa5d49db5933b50dbc37142b0df8a41b572
William R Peters <31421828+peterswi@users.noreply.github.com>
2020-10-26 18:58:34 -0400

    counting shit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 
 d3.csv('data/unemployment.csv',d3.autoType).then(data=>{
     console.log(data)
-
+    totalCount=[]
+    data.forEach(element => {
+      //need to find all the sums
+      total=element["Agriculture"]+element["Wholesale and Retail Trade"]+element["Manufacturing"]+element["Leisure and hospitality"]+element["Business services"]+element["Construction"]+element["Education and Health"]+element["Government"]+element["Finance"]+element["Self-employed"]+element["Other"]+element["Transportation and Utilities"]+element["Information"]+element["Mining and Extraction"]
+      data.push(total)
+      totalCount.push(total)
+    });
+    console.log(data)
+    console.log(totalCount)
+    
 
 })
