57d0aa6bc811a82529c82551d09a1cc05ffcc40d
William R Peters <31421828+peterswi@users.noreply.github.com>
2020-10-27 12:31:54 -0400

    total count confirmed

--- js/script.js
+++ js/script.js
@@ -5,11 +5,10 @@ d3.csv('data/unemployment.csv',d3.autoType).then(data=>{
     data.forEach(element => {
       //need to find all the sums
       total=element["Agriculture"]+element["Wholesale and Retail Trade"]+element["Manufacturing"]+element["Leisure and hospitality"]+element["Business services"]+element["Construction"]+element["Education and Health"]+element["Government"]+element["Finance"]+element["Self-employed"]+element["Other"]+element["Transportation and Utilities"]+element["Information"]+element["Mining and Extraction"]
-      data.push(total)
-      totalCount.push(total)
+      element['total']=total
+      
     });
     console.log(data)
-    console.log(totalCount)
-    
+ 
 
 })
