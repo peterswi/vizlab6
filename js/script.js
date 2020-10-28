ccfcd7211330e684bbebf7df161fb446a8cb8d08
William R Peters <31421828+peterswi@users.noreply.github.com>
2020-10-27 22:07:00 -0400

    bulldozing ahead

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 
 
 d3.csv('data/unemployment.csv',d3.autoType).then(data=>{
-    console.log(data)
+    
     let total
     data.forEach(element => {
       //need to find all the sums
@@ -13,7 +13,8 @@ d3.csv('data/unemployment.csv',d3.autoType).then(data=>{
       
     });
     console.log(data)
- 
+    const chart=AreaChart('.areachart')
+    chart.update(data)
 
 })
 
@@ -23,8 +24,8 @@ function AreaChart(container){
 
   // initialization
   const margin = { top: 20, right: 30, bottom: 30, left: 50 };
-  const width = 450 - margin.left - margin.right;
-  const height = 300 - margin.top - margin.bottom;
+  const width = 600 - margin.left - margin.right;
+  const height = 400 - margin.top - margin.bottom;
 
   const svg = d3
     .select(".areachart")
@@ -41,9 +42,46 @@ function AreaChart(container){
     const yScale=d3.scaleLinear()
       .range([height, 0]) 
 
-	function update(data){ 
+    const xAxis = d3.axisBottom()
+      .scale(xScale)
+    
+    const yAxis = d3.axisLeft()
+      .scale(yScale)
+    
+    svg.append("g")
+      .attr("class", "axis x-axis")
+    svg.append("g")
+      .attr("class", "axis y-axis")
 
-		// update scales, encodings, axes (use the total count)
+   
+        
+
+
+    svg.append("path")
+        .attr('class', 'path')
+
+	  function update(data){ 
+
+    // update scales, encodings, axes (use the total count)
+      xScale.domain(d3.extent(data, d => d.date))
+
+      yScale.domain(d3.extent(data, d => d.total))
+
+      let area = d3.area()
+            .x(d => xScale(d.date))
+            .y1(d => yScale(d.total))
+            .y0(d => yScale.range()[0])
+        
+      d3.select('.path')
+          .datum(data)
+          .attr("d", area)
+
+      svg.select('.x-axis')
+        .attr("transform", `translate(0, ${height})`)
+        .call(xAxis)
+
+      svg.select('.y-axis')
+        .call(yAxis)
 		
 	}
 
